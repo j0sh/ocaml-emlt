528adb05be1ef5bf924823f9271af6068009a05e
Josh Allmann <joshua.allmann@gmail.com>
2015-01-21 09:59:29 -0800

    main: Collapse consecutive char tokens.

--- main.ml
+++ main.ml
@@ -9,6 +9,17 @@ open Parser
 open Types
 open Printf
 
+let collapse toks =
+  (* takes [Ch a; Ch b; Ch c;] and converts into [String abc] *)
+  let f (buf, acc) = function
+    | Ch a -> Buffer.add_char buf a; (buf, acc)
+    | b -> begin if Buffer.length buf > 0 then
+              ((Buffer.create 200), b::(String (Buffer.contents buf))::acc)
+            else (buf, b::acc)
+      end in
+  let (buf, acc) = List.fold_left f ((Buffer.create 200), []) toks in
+  String (Buffer.contents buf) :: acc |> List.rev
+
 let () =
   let lex = Lexing.from_channel stdin in
   let rec p = function
@@ -16,6 +27,6 @@ let () =
     | Open_p s :: t -> printf "main/open/print: %s\n"s ; p t
     | Open_y s :: t -> printf "main/open/yield %s\n" s; p t
     | String s :: t -> print_endline s; p t
-    | Ch c :: t -> printf "%c" c; p t
+    | Ch c :: t -> (); p t
     | Eof :: t | t -> () in
-  p (prog Lexer.tokens lex)
+  prog Lexer.tokens lex |> collapse |> p
