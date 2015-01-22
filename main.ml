e2cc356f5613575b0ef7b628c2901e36d2f101b6
Josh Allmann <joshua.allmann@gmail.com>
2015-01-21 22:03:28 -0800

    main: Redo previous commit.
    Paramterizing by outchans does not cleanly work with stdlib buffers.

--- main.ml
+++ main.ml
@@ -11,8 +11,6 @@ open Printf
 
 let print = ref false
 
-let oc = ref stdout
-
 let opts = [
   "-p",
   Arg.Set print,
@@ -37,31 +35,33 @@ let yields toks =
   let f acc = function Open_y h -> h::acc | _ -> acc in
   List.fold_left f [] toks |> List.rev
 
-let prologue (y : string list) : unit =
+let prologue (y : string list) emit : unit =
   let f = sprintf "?(%s = fun () -> ())" in
   let z = List.map f y |> String.concat " " in
-  fprintf !oc "let print ?(f = fun s -> print_string s; flush stdout) %s param
-= " z
+  let z = sprintf "let print ?(f = fun s -> print_string s; flush stdout) %s param
+= " z in
+  emit z
 
-let epilogue () = fprintf !oc "()\n"
+let epilogue emit = emit "()\n"
 
-let print_printer () = fprintf !oc "let () = print ()\n"
+let print_printer emit = emit  "let () = print ()\n"
 
+let execute emit = ()
 
 let () =
   Arg.parse opts (fun x -> ()) usage;
   let lex = Lexing.from_channel stdin in
-  let emit = fprintf !oc in
+  let emit = print_string in
   let rec p = function
-    | Open s :: t -> emit "%s\n" s; p t
-    | Open_p s :: t -> emit "let () = f (%s) in \n"s ; p t
-    | Open_y s :: t -> emit "let () = %s () in \n" s; p t
-    | String s :: t -> emit "let () = f \"%s\" in \n" s; p t
-    | Fn (f, s) :: t -> fprintf !oc "let %s () = f \"%s\" in\n" f s; p t
+    | Open s :: t -> emit (sprintf "%s\n" s); p t
+    | Open_p s :: t -> emit (sprintf "let () = f (%s) in\n" s) ; p t
+    | Open_y s :: t -> emit (sprintf "let () = %s () in\n" s); p t
+    | String s :: t -> emit (sprintf "let () = f \"%s\" in\n" s); p t
+    | Fn (f, s) :: t -> emit (sprintf "let %s () = f \"%s\" in\n" f s); p t
     | Ch c :: t -> (); p t
     | Eof :: t | t -> () in
   let toks = prog Lexer.tokens lex |> collapse in
-  prologue (yields toks);
+  prologue (yields toks) emit;
   p toks;
-  epilogue ();
-  if !print then print_printer ();
+  epilogue emit;
+  if !print then print_printer emit;
